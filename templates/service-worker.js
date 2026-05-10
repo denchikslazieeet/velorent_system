c0fb8a3b9ed2cb197d1553f708ec1e63f99d007b
denchikslazieeet <denchikslazieeet@users.noreply.github.com>
2026-05-10 17:29:56 +0900

    Add PWA install support

---
+++ templates/service-worker.js
@@ -0,0 +1,56 @@
+{% load static %}
+const CACHE_NAME = "velorent-pwa-v2";
+const OFFLINE_URL = "{% url 'offline' %}";
+const STATIC_ASSETS = [
+  OFFLINE_URL,
+  "{% static 'css/style.css' %}?v=20260510-6",
+  "{% static 'img/velorent-icon.svg' %}",
+  "{% static 'img/pwa-icon-192.png' %}",
+  "{% static 'img/pwa-icon-512.png' %}"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(event.request.url);
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+    );
+    return;
+  }
+
+  if (url.origin === self.location.origin && url.pathname.startsWith("/static/")) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        if (cached) {
+          return cached;
+        }
+        return fetch(event.request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        });
+      })
+    );
+  }
+});
--- velorent/urls.py
+++ velorent/urls.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic import TemplateView
 from dashboard.views import HomePageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("manifest.webmanifest", TemplateView.as_view(
+        template_name="manifest.webmanifest",
+        content_type="application/manifest+json",
+    ), name="webmanifest"),
+    path("service-worker.js", TemplateView.as_view(
+        template_name="service-worker.js",
+        content_type="application/javascript",
+    ), name="service-worker"),
+    path("offline/", TemplateView.as_view(template_name="offline.html"), name="offline"),
     path("", HomePageView.as_view(), name="home"),
     path("", include("dashboard.urls")),
     path("accounts/", include("accounts.urls")),
